f13ba914105cbe7c82de4a893c6a165eacaed242
q3640850400 <910626231@qq.com>
2018-04-10 22:27:52 -0700

    have a rest

--- app.js
+++ app.js
@@ -17,15 +17,24 @@ const wss= new WebSocketServer({
 wss.on('connection', function (ws,req) {
     console.log(`[SERVER] 已连接`);
     if(WaitingRooms.size===0){
-        WaitingRooms.set(req.headers.flueid,new Set())//如果等候室没有房间，就创建一个新房间
+        var room={
+            tetris_20:new Array(),
+            RoomNo:0,
+            gametime:0,
+            gamestate:0,
+            playerstate:new Map(),
+            links:new Set()
+        }
+        var i=0;while(i<20){room.tetris_20.push(parseInt(Math.random()*8));i++;}
+        WaitingRooms.set(req.headers.flueid,room)//如果等候室没有房间，就创建一个新房间
     }
     WaitingRooms.forEach((val,key)=>{
-        if(val.size<maxmember){//如果等候室有房间，就加入一个房间
-            val.add(ws)
+        if(val.links.size<maxmember){//如果等候室有房间，就加入一个房间
+            val.links.add(ws)
             ws.send(`你的房间号: ${key}`, (err) => {
                 if (err) {console.log(`[SERVER] error: ${err}`);}
              });
-            if(val.size>=maxmember){//把满的房间从等候室放到游戏室
+            if(val.links.size>=maxmember){//把满的房间从等候室放到游戏室
                 GamingRooms.set(key,val)
                 WaitingRooms.delete(key)
             }            
@@ -33,8 +42,25 @@ wss.on('connection', function (ws,req) {
         }
     })
     console.log(`当前等待房间数：${WaitingRooms.size}  游戏房间数：${GamingRooms.size}`)
+
     ws.on('message', function (message) {
         console.log(`[SERVER] Received: ${message}`);
+        var msg=JSON.parse(message)
+        switch(msg.code){
+            case 'ready0':{
+                console.log('ready0')
+                break
+            }
+            case 'ready1':{
+                
+            }
+            case 'score':{}
+            case 'update':{}
+            case 'fail':{}
+            case 'end':{}
+            case 'off':{}
+            default :{break}
+        }
         ws.send(`ECHO: ${message}`, (err) => {
             if (err) {
                 console.log(`[SERVER] error: ${err}`);
