b1611be3378571beee0552a07d67ebfd0e57c6a4
BeKaLo <910626231@qq.com>
2018-05-22 17:54:11 +0800

    sql and chatlogs file

--- app.js
+++ app.js
@@ -11,24 +11,69 @@ const wss = new WebSocketServer({
 })
 
 /*
+ *文件输入输出
+ */
+const fs = require('fs');
+
+/*
  *数据库连接
  */
 const mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '172.21.0.7',
     port: '12315',
+    // host: 'localhost',
+    // port: '3306',
     user: 'root',
     password: 'Yggzs@2018',
     database: 'yychat'
 });
 connection.connect();
 // connection.query("insert into user_information(name,password,create_time) values ('gg','44',now())", function(error, results, fields) {
-// connection.query('delete from user_information', function(error, results, fields) {
+// connection.query('delete from chat_logs', function(error, results, fields) {});
 // connection.query("select * from user_information", function(error, results, fields) {
-connection.query('drop table user_information', function(error, results, fields) {
-    if (error) throw error;
-    console.log('The results is: ', results);
-});
+//每分钟把记录写入数据库
+setInterval(function() {
+    try {
+
+        var d = new Date();
+        var fulldate = d.getFullYear() + '_' + (d.getMonth() + 1) + '_' + d.getDate();
+        fs.readFile('chatlogs/' + fulldate + '.txt', function(err, data) {
+            connection.query("select * from chat_logs where date=?", [
+                    [fulldate]
+                ], function(error, results, fields) {
+                    if (results.length == 0) {
+                        connection.query("insert into chat_logs(date,logs,tag) values (?)", [
+
+                            [fulldate, data, 'test']
+                        ], function(error, results, fields) {
+
+                            if (error) { throw error; }
+                            //console.log('The results is: ', results);
+                        })
+                    } else {
+                        connection.query("update chat_logs set logs=? where date=?", [
+                            [data],
+                            [fulldate]
+                        ], function(error, results, fields) {})
+                    }
+                })
+                // connection.query("insert into chat_logs(date,logs,tag) values (?)", [
+
+            //     [fulldate, data, 'test']
+            // ], function(error, results, fields) {
+
+            //     if (error) { throw error; }
+            //     //console.log('The results is: ', results);
+            // })
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}, 60000);
+
 
 
 /*
@@ -67,6 +112,15 @@ wss.on('connection', function(ws, req) {
                     }
                 case "msg":
                     {
+
+                        // 写入临时文件
+                        var d = new Date();
+                        var fulldate = d.getFullYear() + '_' + (d.getMonth() + 1) + '_' + d.getDate();
+                        fs.appendFile('chatlogs/' + fulldate + '.txt', inmsg.data, function(err, fd) {
+                            if (err) {
+                                return console.error(err);
+                            }
+                        });
                         wss.clients.forEach(client => {
                             client.send(message);
                         });
